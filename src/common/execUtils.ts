3a32725b282bd2ba08a7da40d70a55889c9d3e00
Renan Rodrigues dos Santos <renan.engmec@gmail.com>
2026-03-09 11:37:52 -0300

    Update quoteArgs

--- src/common/execUtils.ts
+++ src/common/execUtils.ts
@@ -1,10 +1,21 @@
-function quoteArg(arg: string): string {
-    if (arg.indexOf(' ') >= 0 && !(arg.startsWith('"') && arg.endsWith('"'))) {
-        return `"${arg}"`;
+export function quoteStringIfNecessary(arg: string): string {
+    // Always return if already quoted to avoid double-quoting
+    if (arg.startsWith('"') && arg.endsWith('"')) {
+        return arg;
     }
-    return arg;
+
+    // Don't quote single shell operators/special characters
+    if (arg.length === 1 && /[&|<>;()[\]{}$]/.test(arg)) {
+        return arg;
+    }
+
+    // Quote if contains common shell special characters that are problematic across multiple shells
+    // Includes: space, &, |, <, >, ;, ', ", `, (, ), [, ], {, }, $
+    const needsQuoting = /[\s&|<>;'"`()\[\]{}$]/.test(arg);
+
+    return needsQuoting ? `"${arg}"` : arg;
 }
 
 export function quoteArgs(args: string[]): string[] {
-    return args.map(quoteArg);
+    return args.map(quoteStringIfNecessary);
 }
